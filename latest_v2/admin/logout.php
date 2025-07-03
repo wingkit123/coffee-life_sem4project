ac6c5cea624f8189fed6af9bb357a77f720a0f4a
wingkit123 <83150149+wingkit123@users.noreply.github.com>
2025-07-03 13:43:29 +0800

    Delete latest_v2 directory

--- latest_v2/admin/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-// admin/logout.php
-session_start();
-$_SESSION = array(); // Unset all of the session variables
-session_destroy(); // Destroy the session
-header("Location: login.php"); // Redirect to login page
-exit;
-?>
--- latest_v2/user_logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-// user_logout.php
-session_start(); // Start the session
-$_SESSION = array(); // Unset all of the session variables
-session_destroy(); // Destroy the session
-header("Location: user_login.php"); // Redirect to user login page
-exit;
-?>
